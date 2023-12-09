eafc7925a0cde6048bcb0ff62f7ef673576d9bdb
steven sanchez <1steven.nathan.sanchez@gmail.com>
2023-12-09 07:50:02 -0600

    Se definio el modelo Dispositivo.

--- generacion_energia_monitoring/monitoring/models.py
+++ generacion_energia_monitoring/monitoring/models.py
@@ -13,3 +13,14 @@ class StatusDispositivo(models.Model):
 
     def __str__(self):
         return self.descripcion
+
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=255)
+    tipo_dispositivo = models.ForeignKey(TipoDispositivo, on_delete=models.CASCADE)
+    fecha_alta = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    potencia_actual = models.FloatField(null=True, blank=True)
+    status_dispositivo = models.ForeignKey(StatusDispositivo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
