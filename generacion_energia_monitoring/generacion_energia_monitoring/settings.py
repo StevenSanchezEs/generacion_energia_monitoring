b1bbd3bd4daa15f572dec99b6e8a1bd21758789b
steven sanchez <1steven.nathan.sanchez@gmail.com>
2023-12-09 07:08:02 -0600

    Se configuraron las credenciales para la base de datos

--- generacion_energia_monitoring/generacion_energia_monitoring/settings.py
+++ generacion_energia_monitoring/generacion_energia_monitoring/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'generacion_energia_monitoring.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'monitoring',
+        'USER': 'root',
+        'PASSWORD': '17071969200',
+        'HOST': '192.168.1.217',
+        'PORT': 3306,
     }
 }
 
