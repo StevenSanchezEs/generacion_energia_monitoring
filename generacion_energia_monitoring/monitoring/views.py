cd6e77353a73ec767f41e5ef06a983d638d4b225
steven sanchez <1steven.nathan.sanchez@gmail.com>
2023-12-09 08:21:24 -0600

    Se definio TipoDispositivoViewSet.

--- generacion_energia_monitoring/monitoring/views.py
+++ generacion_energia_monitoring/monitoring/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from datetime import datetime
+from rest_framework.decorators import authentication_classes, permission_classes
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from django.db.models import Sum
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import TipoDispositivo, StatusDispositivo, Dispositivo, Lectura, Mantenimiento
+from .serializers import TipoDispositivoSerializer, StatusDispositivoSerializer, DispositivoSerializer, LecturaSerializer, MantenimientoSerializer
 
 # Create your views here.
+
+class TipoDispositivoViewSet(viewsets.ModelViewSet):
+    queryset = TipoDispositivo.objects.all()
+    serializer_class = TipoDispositivoSerializer
+    
