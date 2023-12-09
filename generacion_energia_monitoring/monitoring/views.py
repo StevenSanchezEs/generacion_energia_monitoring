ab2bd0ea0d1e405b06cf2590c14b742d06e177e4
steven sanchez <1steven.nathan.sanchez@gmail.com>
2023-12-09 08:25:45 -0600

    Se definio función update.

--- generacion_energia_monitoring/monitoring/views.py
+++ generacion_energia_monitoring/monitoring/views.py
@@ -23,3 +23,35 @@ class DispositivoViewSet(viewsets.ModelViewSet):
     queryset = Dispositivo.objects.all()
     serializer_class = DispositivoSerializer
     
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+
+        # Obtiene el estado del dispositivo antes de la actualización
+        old_status = instance.status_dispositivo
+
+        # Actualiza el dispositivo
+        self.perform_update(serializer)
+
+        # Verifica si el estado del dispositivo ha cambiado a "En mantenimiento"
+        if instance.status_dispositivo != old_status and instance.status_dispositivo.descripcion == "En mantenimiento":
+            # Crea un registro de mantenimiento
+            mantenimiento = Mantenimiento.objects.create(dispositivo=instance)
+
+            # Obtén todos los registros de mantenimiento asociados al dispositivo actual
+            registros_mantenimiento = Mantenimiento.objects.filter(dispositivo=instance)
+
+            # Serializa los registros de mantenimiento
+            serializer_mantenimiento = MantenimientoSerializer(registros_mantenimiento, many=True)
+
+            # Devuelve la respuesta con los registros de mantenimiento
+            return Response({
+                "dispositivo": serializer.data,
+                "registros_mantenimiento": serializer_mantenimiento.data
+            })
+
+        return Response(serializer.data)
+    
+    
